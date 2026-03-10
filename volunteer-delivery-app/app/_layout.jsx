6a80b02271e6c47be4efc34aa8bfa7a1daaf6990
NhienHong <nhienbernie@gmail.com>
2026-03-10 00:35:55 -0400

    consolidate layout files into jsx

--- volunteer-delivery-app/app/_layout.jsx
+++ volunteer-delivery-app/app/_layout.jsx
@@ -3,24 +3,12 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen
-        name="volunteer-signup"
-        options={{ title: "Volunteer Sign Up" }}
-      />
-      <Stack.Screen
-<<<<<<< HEAD
-        name="volunteer-signin"
-        options={{ title: "Volunteer Sign In" }}
-=======
-        name="volunteer-dashboard"
-        options={{ title: "Volunteer Dashboard" }}
-      />
-      <Stack.Screen
-        name="confirm-delivery"
-        options={{ title: "Confirm Delivery" }}
->>>>>>> origin/Beck-sprint2
-      />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="mode-select" options={{ headerShown: false }} />
+      <Stack.Screen name="volunteer-options" options={{ title: "Volunteer Portal" }} />
+      <Stack.Screen name="volunteer-signup" options={{ title: "Volunteer Sign Up" }} />
+      <Stack.Screen name="volunteer-signin" options={{ title: "Volunteer Sign In" }} />
+      <Stack.Screen name="requester" options={{ title: "Request Food Assistance" }} />
     </Stack>
   );
 }
